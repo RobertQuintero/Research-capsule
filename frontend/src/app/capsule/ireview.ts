bf00e59331a6af7de4de5db6d790f680b3db6c74
Robert Timbang Quintero <90501105+Robebaebae@users.noreply.github.com>
2023-05-14 04:12:55 +0800

    Frontend Deleted

--- frontend/src/app/capsule/ireview.ts
+++
@@ -1,9 +0,0 @@
-import { IUser } from "../user/iuser"
-export interface Review {
-    user_id?: number
-    capsule_id: number
-    grade?: number
-    comment?: string
-    user?: IUser
-    isReviewed?: boolean
-}
